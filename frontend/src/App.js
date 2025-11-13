9c6d24eb3cea49ae42ddd8f36dda089656766a31
Kornelio Geric <geric.kornelio@live.com>
2025-11-13 16:59:43 +0000

    auto-commit for c3d1fa6c-9d4f-4b68-94b2-c098498af998

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1945,18 +1945,16 @@ const Dashboard = () => {
                 )}
 
                 {/* Active Users GPS */}
-                {activeUsers.map((activeUser) => (
+                {activeUsers.map((activeUser) => {
+                  const userInfo = allUsers.find(u => u.id === activeUser.user_id);
+                  const dvdColor = getDepartmentColor(userInfo?.department);
+                  const userIcon = userInfo ? createGpsMarkerIcon(userInfo.role, dvdColor) : activeUserIcon;
+                  
+                  return (
                   <Marker
                     key={activeUser.user_id}
                     position={[activeUser.latitude, activeUser.longitude]}
-                    icon={L.icon({
-                      iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-                      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-                      iconSize: [25, 41],
-                      iconAnchor: [12, 41],
-                      popupAnchor: [1, -34],
-                      shadowSize: [41, 41]
-                    })}
+                    icon={userIcon}
                   >
                     <Popup>
                       <div className="p-2">
